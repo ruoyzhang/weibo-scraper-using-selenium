978b6577a188aecac7778a051b287a57a60636b7
Ruoy <ruoyzhang@gmail.com>
2019-04-17 12:54:29 +0200

    class done, testing, ran into browser issue?
    the selenium driven firefox behaves very differently than the native firefox, after logging into the sina, when navigating to weibo, we became 'not logged in' again
    
    This is new behaviour, during the prototyping phase this worked perfectly

--- weibo_scrapper.py
+++ weibo_scrapper.py
@@ -9,7 +9,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys        
 import selenium.webdriver.support.ui as ui        
 from selenium.webdriver.common.action_chains import ActionChains
-
+from datetime import datetime
+from tqdm import tqdm
+import pickle
 
 #-------------------------------------------------------------------------------------
 # ||||||||||||||||||||||||||||||||||||||||||||||||
@@ -27,13 +29,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 class weibo_scrapper():
 
-	def __init__(self, begin_date, end_date, usernamem, password, search_keyword):
+	def __init__(self, begin_date, end_date, username, password, search_keyword, save_dir):
 		"""
 		begin_date & end_date: date limits for the research results,
 								they have to be in the format of "YYYY-MM-DD"
 								as is conventional in the Chinese writting sys
 		username & password: user authentication
 		search_keyword: the keyword used for Weibo advanced search
+		save_dir: directory for where we want to save the srapped data
 		"""
 
 		# storing vars as class vars
@@ -81,12 +84,12 @@ class weibo_scrapper():
 			time.sleep(2)
 
 			# getting the cookie used for login in later
-			print(self.driver.current_url)
-			print(self.driver.get_cookies())
-			print('Outputting the cookie info')
-			for cookie in self.driver.get_cookies():
-				for key in cookie:
-					print(key, cookie[key])
+			#print(self.driver.current_url)
+			#print(self.driver.get_cookies())
+			#print('Outputting the cookie info')
+			#for cookie in self.driver.get_cookies():
+			#	for key in cookie:
+			#		print(key, cookie[key])
 
 			print('login successful')
 
@@ -103,19 +106,30 @@ class weibo_scrapper():
 		The scrapping method
 		"""
 
+		print('logging into Sina')
+
 		# log into Sina
 		self.Login_Weibo()
+
+		print('navigating to Weibo adv search page')
 
 		# navigate to the search interface
 		self.driver.get("http://s.weibo.com/")
 
+		print('waiting for page to load ...')
+
 		# need to wait for the page to load else it won't work
 		time.sleep(10)
 
+		print('specifying search criterion')
+
 		# input the search key and hit enter in order to advance to the advanced search interface
 		item_inp = self.driver.find_element_by_xpath("//input[@type='text']")
-		item_inp.send_keys(key)
+		item_inp.send_keys(self.keyword)
 		item_inp.send_keys(Keys.RETURN)
+
+		# waiting
+		time.sleep(5)
 
 		# now we try to instruct selenium to click on the 'advacned search' bottom
 		self.driver.find_element_by_xpath("//a[@node-type='advsearch']").click()
@@ -154,17 +168,60 @@ class weibo_scrapper():
 		# need to wait again
 		time.sleep(5)
 
+		print('waiting for results to load ...')
 
+		# a loop for all the result pages
+		# first determine the total number of pages (usually 50 but just in case)
+		dropdown_page = driver.find_elements_by_xpath("/html/body/div/div/div/div/div/div/span/ul[@node-type='feed_list_page_morelist']/li")
+		num_pg = len(dropdown_page)
 
+		# storage variables for the tweets and dates
+		all_tweets = []
+		all_dates = []
 
+		print('now scrapping ...')
 
+		# now loop
+		for i in range(num_pg - 1):
 
+			# now get the tweets
+			tweets = driver.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/p[@class='txt' and @node-type='feed_list_content']")
 
+			# retrieve only the text
+			tweets = [tweet.text for tweet in tweets]
 
+			# # the below extracts date
+			dates = driver.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/p[@class='from']")
 
+			# trying to convert string to datetime format
+			# first, replace the chinese characters
+			dates = [date.text.split(' ')[0].replace('年', ' ').replace('月', ' ').replace('日', '') for date in item_tweet_dates]
 
+			# then add year if necessary
+			dates = ['2019 ' + date if len(date) == 5 else date for date in dates]
 
+			# then we convert it to datetime format
+			dates = [datetime.strptime(date, '%Y %m %d') for date in dates]
 
+			# append res to the storage variables
+			all_tweets += tweets
+			all_dates += dates
 
+			print('page ', str(i+1), ' complete')
 
-		# setting up
+			# go to the next page
+			driver.find_element_by_xpath("/html/body/div/div/div/div/div/div/a[@class='next']").click()
+
+			# wait again
+			time.sleep(8)
+
+		print('all pages scrapped, now saving ...')
+
+		# save the results as pickles
+		with open(os.path.join(save_dir, '_'.join([self.end_day, self.end_month, self.end_year, 'text.pickle'])), 'wb') as handle:
+			pickle.dump(all_tweets, handle)
+		with open(os.path.join(save_dir, '_'.join([self.end_day, self.end_month, self.end_year, 'date.pickle'])), 'wb') as handle:
+			pickle.dump(all_dates, handle)
+
+		print('save complete, files can be found at: ', save_dir)
+
